be330c6f5a8a5a7921f4a4d241e9995e640b29a4
bLue <tbdblue@gmail.com>
2018-03-11 13:56:07 +0800

    Add timezone support

--- rc-chrome_extension/js/rc.js
+++ rc-chrome_extension/js/rc.js
@@ -1,3 +1,28 @@
+function preZeroFill(num, size) {
+	if (num >= Math.pow(10, size)) {
+		return num.toString();
+	}
+	else {
+		var str = Array(size + 1).join('0') + num;
+		return str.slice(str.length - size);
+	}
+}
+
+function formatTimeStr(t) {
+	return t.getFullYear() + '-' + preZeroFill(t.getMonth()+1, 2) + '-' + preZeroFill(t.getDate(), 2) +
+		' ' + preZeroFill(t.getHours(), 2) + ':' + preZeroFill(t.getMinutes(), 2) + ':' + preZeroFill(t.getSeconds(), 2);
+}
+
+function formatTimeTZ(str, timezone) {
+	var t = new Date(str + timezone);
+	var tz = parseInt(new Date().getTimezoneOffset()/-60);
+	if(tz >= 0) tz = '+' + tz;
+	return {
+		time: formatTimeStr(t),
+		tz: tz,
+	}
+}
+
 $(function () {
 	$.ajax({
 		url: "https://contests.acmicpc.info/contests.json",
@@ -14,9 +39,10 @@ $(function () {
 				tr.append('<td class="text-left padding-md"><a target="_blank"></a></td>');
 				tr.find('td:last a').text(contest['name']);
 				tr.find('td:last a').attr('href', contest['link']);
+				var starTime = formatTimeTZ(contest['start_time'], '+8');
 				tr.append('<td><span class="start_time"></span><sup class="tz"></sup></td>');
-				tr.find('td:last .start_time').text(contest['start_time']);
-				tr.find('td:last .tz').text('UTC+8');
+				tr.find('td:last .start_time').text(starTime['time']);
+				tr.find('td:last .tz').text('UTC' + starTime['tz']);
 				tr.append('<td></td>');
 				tr.find('td:last').text(contest['week']);
 				var startTime = new Date(contest['start_time'].replace(/-/g, '/'));
